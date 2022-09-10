0f4e952cbe0d9d99bd64bddcfee1f703bb233924
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-10 13:41:58 +0100

    get data

--- main.py
+++ main.py
@@ -6,3 +6,24 @@ import datetime as dt
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 from tensorflow.keras.models import Sequential
+
+crypto_currency = 'BTC'
+against_currency = 'USD'
+
+start  = dt.datetime(2016, 1, 1)
+end = dt.datetime.now()
+
+data = web.DataReader(f'{crypto_currency}-{against_currency}', 'yahoo', start, end)
+
+#Preparation of the data
+
+scaler = MinMaxScaler(feature_range=(0,1))
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
+
+predictio_days = int(input('How many days do you want to predict?: '))
+
+x_train, y_train = [], []
+
+for i in range(predictio_days, len(scaled_data)):
+    x_train.append(scaled_data[i-predictio_days:i, 0])
+    y_train.append(scaled_data[i, 0])
