568514f35f78f35134aae7b9a29d29619f45a3ff
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-10 13:47:58 +0100

    test the model

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from re import T
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -33,3 +34,12 @@ x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
 #Create the neural network
 model = Sequential()
+model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(units=50))
+model.add(Dropout(0.2))
+model.add(Dense(units=1))
+
+model.compile(optimizer='adam', loss='mean_squared_error')
